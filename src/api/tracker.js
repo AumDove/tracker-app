e61d7a2be5e00c7cf8b731d1f6f263e2dc5c8da9
aumdove <aumdove13@gmail.com>
2020-03-26 14:44:14 -0500

    added files

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL: 'http://bb2cb89e.ngrok.io'
+});
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,64 @@
+import React, { useState, useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
+
+const SignupScreen = ({ navigation }) => {
+	const { state, signup } = useContext(AuthContext);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	return (
+		<View style={styles.container}>
+			<Spacer>
+				<Text h3>Sign Up for Tracker</Text>
+			</Spacer>
+			<Spacer />
+			<Input
+				autoCapitalize='none'
+				autoCorrect={false}
+				label='Email'
+				value={email}
+				onChangeText={setEmail}
+			/>
+			<Spacer />
+			<Input
+				secureTextEntry
+				autoCapitalize='none'
+				autoCorrect={false}
+				label='Password'
+				value={password}
+				onChangeText={setPassword}
+			/>
+			<Spacer />
+
+			{state.errorMessage ? (
+				<Text style={styles.errorMessage}>{state.errorMessage}</Text>
+			) : null}
+			<Spacer>
+				<Button title='Sign Up' onPress={() => signup({ email, password })} />
+			</Spacer>
+		</View>
+	);
+};
+
+SignupScreen.navigationOptions = {
+	header: null
+};
+
+const styles = StyleSheet.create({
+	container: {
+		marginBottom: 150,
+		flex: 1,
+		justifyContent: 'center'
+	},
+	errorMessage: {
+		fontSize: 16,
+		color: 'red',
+		marginLeft: 15,
+		marginTop: 15
+	}
+});
+
+export default SignupScreen;
